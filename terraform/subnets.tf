fe757316c2e6694d874b9539e308c5b566bdfa8c
Shivani Saurabh <saurabh.shivani3@gmail.com>
2026-06-15 12:09:02 +0530

    Fix EKS role and subnet AZ

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.capstone_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-1a"
+  availability_zone       = "us-west-1c"
   map_public_ip_on_launch = true
 
   tags = {
